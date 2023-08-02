a82446ffdd888f413803bd67717cb91b10a9e497
Sheremet1 <eparis@atlassian.com>
2023-08-02 22:25:31 +0300

    core

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,15 +1,15 @@
-import sorting from "../sort";
+import sorting from '../sort';
 
-test("sorting array", () => {
+test('sorting array', () => {
   const result = sorting([
-    { name: "мечник", health: 10 },
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ]);
 
   expect(result).toStrictEqual([
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-    { name: "мечник", health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ]);
 });
