795fe8139f6c28fbe0b53d27e9f338044ba28ad4
AlexanderSheremet <118627009+AlexanderSheremet@users.noreply.github.com>
2023-08-07 21:18:40 +0300

    Update test.js

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,15 +1,30 @@
-import sorting from '../sort';
+import getStatus from '../sort';
 
-test('sorting array', () => {
-  const result = sorting([
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ]);
+const hero1 = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
 
-  expect(result).toStrictEqual([
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ]);
+const hero2 = [
+  { name: 'мечник', health: 100 },
+  { name: 'маг', health: 10 },
+  { name: 'лучник', health: 80 },
+];
+
+const hero3 = [
+  { name: 'мечник', health: 100 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+test('sorting hero1', () => {
+  expect(hero1.sort(getStatus)).toEqual(hero1);
+});
+
+test('sorting hero2', () => {
+  expect(hero2.sort(getStatus)).toEqual(hero2);
+});
+
+test('sorting hero3', () => {
+  expect(hero3.sort(getStatus)).toEqual(hero3);
 });
